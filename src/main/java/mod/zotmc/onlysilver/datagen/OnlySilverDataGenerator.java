512715c3283ac7cbdba0a8547c81126e00daa625
Sinhika <sinhika@republicofnewhome.org>
2020-08-04 15:03:26 -0500

    added silver axe and silver pickaxe to data gen

--- src/main/java/mod/zotmc/onlysilver/datagen/OnlySilverDataGenerator.java
+++ src/main/java/mod/zotmc/onlysilver/datagen/OnlySilverDataGenerator.java
@@ -5,9 +5,13 @@ import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 import java.util.function.Consumer;
 
 import mod.zotmc.onlysilver.OnlySilver;
+import mod.zotmc.onlysilver.init.ModItems;
+import mod.zotmc.onlysilver.init.ModTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -51,8 +55,45 @@ public class OnlySilverDataGenerator
 
         protected void registerRecipes(Consumer<IFinishedRecipe> consumer)
         {
+            registerMiscRecipes(consumer);
+            registerToolRecipes(consumer);
+            registerArmorRecipes(consumer);
+            registerFurnaceRecipes(consumer);
         } // end registerRecipes() 
         
+        protected void registerToolRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+            // axe
+            ShapedRecipeBuilder.shapedRecipe(ModItems.silver_axe.get())
+                .key('S', ModTags.Items.INGOTS_SILVER)
+                .key('T', Tags.Items.RODS_WOODEN)
+                .patternLine("SS")
+                .patternLine("ST")
+                .patternLine(" T")
+                .addCriterion("has_item", hasItem(ModTags.Items.INGOTS_SILVER))
+                .build(consumer);
+            // pickaxe
+            ShapedRecipeBuilder.shapedRecipe(ModItems.silver_pickaxe.get())
+                .key('S', ModTags.Items.INGOTS_SILVER)
+                .key('T', Tags.Items.RODS_WOODEN)
+                .patternLine("SSS")
+                .patternLine(" T ")
+                .patternLine(" T ")
+                .addCriterion("has_item", hasItem(ModTags.Items.INGOTS_SILVER))
+                .build(consumer);
+        } // end registerToolRecipes()
+        
+        protected void registerArmorRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        }
+        
+        protected void registerFurnaceRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        }
+        
+        protected void registerMiscRecipes(Consumer<IFinishedRecipe> consumer)
+        {
+        }
     } // end subclass OnlySilverDataGenerator$Recipes.
     
 } // end-class OnlySilverDataGenerator
---
+++ src/main/java/mod/zotmc/onlysilver/init/ModTags.java
@@ -0,0 +1,44 @@
+package mod.zotmc.onlysilver.init;
+
+import mod.zotmc.onlysilver.OnlySilver;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+
+public class ModTags
+{
+
+    public static class Items
+    {
+        public static final Tag<Item> INGOTS_SILVER = forgeTag("ingots/silver");
+        public static final Tag<Item> NUGGETS_SILVER = forgeTag("nuggets/silver");
+        public static final Tag<Item> BLOCK_SILVER = forgeTag("storage_blocks/silver");
+        
+//        private static Tag<Item> tag(String name) {
+//            return new ItemTags.Wrapper(new ResourceLocation(OnlySilver.MODID, name));
+//        }
+
+        private static Tag<Item> forgeTag(String name) {
+            return new ItemTags.Wrapper(new ResourceLocation("forge", name));
+        }
+    } // end class Items
+    
+    
+    public static class Blocks
+    {
+        public static final Tag<Block> BLOCK_SILVER = forgeTag("storage_blocks/silver");
+        
+//        private static Tag<Block> tag(String name) {
+//            return new BlockTags.Wrapper(new ResourceLocation(OnlySilver.MODID, name));
+//        }
+
+        private static Tag<Block> forgeTag(String name) {
+            return new BlockTags.Wrapper(new ResourceLocation("forge", name));
+        }
+    } // end class Blocks
+
+    
+} // end class ModTags
