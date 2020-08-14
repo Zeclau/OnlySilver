4317627661bf93957e3c2ecfdbdc1ff365aba18e
Sinhika <sinhika@republicofnewhome.org>
2020-08-14 16:00:17 -0500

    knocked out a few more easy defaults for the enchants

--- src/main/java/mod/zotmc/onlysilver/enchant/IncantationEnchantment.java
+++ src/main/java/mod/zotmc/onlysilver/enchant/IncantationEnchantment.java
@@ -1,8 +1,10 @@
 package mod.zotmc.onlysilver.enchant;
 
+import mod.zotmc.onlysilver.api.OnlySilverRegistry;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 
 public class IncantationEnchantment extends Enchantment
 {
@@ -12,4 +14,37 @@ public class IncantationEnchantment extends Enchantment
         super(Rarity.RARE, EnchantmentType.DIGGER, new EquipmentSlotType[] {EquipmentSlotType.MAINHAND});
     }
 
+    @Override
+    public int getMaxLevel()
+    {
+        return 2;
+    }
+
+    @Override
+    public int getMinEnchantability(int enchantmentLevel)
+    {
+        return 15 + 9 * (enchantmentLevel - 1);
+    }
+
+    @Override
+    public int getMaxEnchantability(int enchantmentLevel)
+    {
+        return super.getMaxEnchantability(enchantmentLevel) + 50;
+    }
+
+    /**
+     * only allow this enchantment on silver items.
+     */
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack)
+    {
+        return (OnlySilverRegistry.isSilverEquip(stack) && super.canApplyAtEnchantingTable(stack));
+    }
+
+    @Override
+    public boolean isAllowedOnBooks()
+    {
+        return false;
+    }
+    
 } // end class
