b3a808eb2b1743f39934608bfca59763e66dd3dc
Sinhika <sinhika@republicofnewhome.org>
2021-02-08 12:14:26 -0600

    added python script to create Silent's Mechanisms crusher recipes

---
+++ scripts/generate_silents_recipes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# scripts are from mod_utils project on my GitHub.
+
+ID='onlysilver'
+NAME='OnlySilver'
+TOPDIR=`pwd`
+PROJNAME=`basename $TOPDIR`
+if [ $PROJNAME != $NAME ]; then
+    echo "Running in wrong directory!"
+    exit 1
+fi
+
+
+# recipes
+TARGETDIR=${TOPDIR}/src/main/resources/data/${ID}/recipes
+if [ ! -d $TARGETDIR ]; then
+    mkdir -p $TARGETDIR
+fi
+cd $TARGETDIR
+
+# Silent's Mechanisms recipes
+# crusher -- ingots
+make_silents_recipes.py --type=crusher --ingredient="${ID}:silver_ingot" \
+    --result="${ID}:silver_dust" silver_dust_from_ingot
+
+# crusher -- ore
+make_silents_recipes.py --type=crusher --ticks=400 --ingredient='${ID}:silver_ore' \
+    --result '${ID}:crushed_silver_ore,2' 'minecraft_cobblestone,0.1' \
+    -- crushed_silver_ore
+
+# crusher -- ore chunks
+make_silents_recipes.py --type=crusher --ingredient='${ID}:crushed_silver_ore' \
+    --result '${ID}:silver_dust' '${ID}:silver_dust,0.1' \
+    -- silver_dust
+
